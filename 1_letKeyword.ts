920f3f508981e752d9a63771880121063059354c
Marcos Rosada <ochild@gmail.com>
2017-06-18 14:34:17 -0300

    Let keyword

---
+++ 1_letKeyword.ts
@@ -0,0 +1,22 @@
+let message:string = "Hi";
+
+{
+    let message:string = "Bye";
+    console.log("Local:", message ); // Bye
+}
+
+console.log("Global:", message); // Hi
+console.log("");
+
+var fs = [];
+
+//for (var i = 0; i < 10; i++) {
+for (let i = 0; i < 10; i++) {
+    fs.push( function () {
+        console.log(i);
+    });
+}
+
+fs.forEach( function(f) {
+    f(); // 0 ... 10
+});
