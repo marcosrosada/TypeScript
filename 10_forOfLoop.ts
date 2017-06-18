b10d116c925a1f34817f4b55185361d0cb6672ee
Marcos Rosada <ochild@gmail.com>
2017-06-18 14:54:50 -0300

    For of loop

---
+++ 10_forOfLoop.ts
@@ -0,0 +1,16 @@
+let colors = ['Red', 'Blue', 'Green'];
+
+for (let index in colors) {
+    console.log('In', colors[index]);
+}
+
+for (let color of colors) {
+    console.log('Of', color);
+}
+
+
+let letters = 'ABC';
+
+for (let letter of letters) {
+    console.log('Of', letter);
+}
