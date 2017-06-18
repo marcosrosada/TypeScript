edf70328213a7ce08827b0e656b48ef066a4fa90
Marcos Rosada <ochild@gmail.com>
2017-06-18 14:40:00 -0300

    Rename

--- this.ts
+++
@@ -1,9 +0,0 @@
-var employee = {
-    id: 1,
-    greet: function () {
-        //setTimeout( function () { console.log(this.id), 1000 }); // undefined
-        setTimeout( () => { console.log(this.id), 1000 }); // 1
-    }
-};
-
-employee.greet();
