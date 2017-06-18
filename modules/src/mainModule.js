0efa43fe9275698b0e84a12f0efa372cf80d1d8b
Marcos Rosada <ochild@gmail.com>
2017-06-18 16:57:49 -0300

    Module Functions

--- modules/src/mainModule.js
+++ modules/src/mainModule.js
@@ -1,8 +1,17 @@
 import { firstName, lastName as last, Person } from './moduleVariableB.js'
 import name from './moduleVariableDefault.js'
 
-Person.name = 'Marcos Rosada from Module A';
+import { sum, mult } from './moduleFunction.js'
+
 
 console.log(`${firstName} ${last}`);
-console.log( Person.name );
+
+Person.name = 'Marcos Rosada';
+console.log('from Module A:', Person.name );
+
 console.log( name );
+
+console.log('');
+
+sum(1, 2) // 3
+mult(1, 2) // 2
---
+++ modules/src/moduleFunction.js
@@ -0,0 +1,7 @@
+export function sum(x, y) {
+  console.log('SUM() From function module:', x + y);
+}
+
+export function mult(x, y) {
+  console.log('MULT() From function module:', x * y);
+}
