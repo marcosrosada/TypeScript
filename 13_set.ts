1e34cd721fce60c9f0cd72a3383c2141ad3ded28
Marcos Rosada <ochild@gmail.com>
2017-06-18 18:16:47 -0300

    Set

---
+++ 13_set.ts
@@ -0,0 +1,17 @@
+let mySet = new Set();
+let obj1 = {};
+let obj2 = {};
+
+mySet.add("Hello");
+mySet.add(1);
+mySet.add(1);
+mySet.add(obj1);
+mySet.add(obj2);
+console.log('My Set Size:', mySet.size);
+
+
+let newSet = new Set([1, 2, 3, 4, 4, 4]);
+console.log('New Set Size:', newSet.size);
+
+newSet.delete(1);
+console.log('New Set Size:', newSet.size);
