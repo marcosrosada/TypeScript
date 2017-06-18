25f24893befcd43c28a5184a2df7523fed89d72e
Marcos Rosada <ochild@gmail.com>
2017-06-18 12:06:52 -0300

    Default Parameters

---
+++ restParameters.ts
@@ -0,0 +1,26 @@
+let displayColors = (message, ...colors) => {
+    console.log(message);
+
+    for (let i in colors) {
+        console.log(colors[i]);
+    }
+}
+
+let message = 'List of Colors';
+
+displayColors(message, 'Red');
+displayColors(message, 'Red', 'Blue');
+displayColors(message, 'Red', 'Blue', 'Green');
+
+
+console.log("");
+
+function sum(...numbers) {
+    let result = 0
+    numbers.forEach((number) => {
+        result += number
+    });
+    console.log('Result', result );
+}
+
+sum(1, 2, 3, 4, 5) // 15
